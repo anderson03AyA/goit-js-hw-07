355df1b1478cb042f0606e3399f2014c1de2015b
anderson03AyA <andersonsolarte03@gmail.com>
2023-03-27 20:51:53 -0500

    tarea 7

---
+++ src/js/01-gallery.js
@@ -0,0 +1,57 @@
+import { galleryItems } from "./gallery-items.js";
+
+const gallery = document.querySelector(".gallery");
+
+function createGallery(items) {
+  return items
+    .map(
+      (item) => `
+     <li class="gallery__item">
+       <a class="gallery__link" href="${item.original}">
+         <img
+           class="gallery__image"
+           src="${item.preview}"
+           data-source="${item.original}"
+           alt="${item.description}"
+         />
+       </a>
+     </li>`
+    )
+    .join("");
+}
+
+gallery.innerHTML = createGallery(galleryItems);
+const addGallery = createGallery(galleryItems);
+
+gallery.innerHTML = addGallery;
+
+gallery.addEventListener("click", clickOnImage);
+
+function clickOnImage(imageAction) {
+  action(imageAction);
+
+  if (imageAction.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${imageAction.target.dataset.source}" width="800" height="600">`
+  );
+  instance.show();
+
+  //    se agrega un eventlistener; que cuando se pulse la tecla ESCAPE se cierre la imagen que esta previamente en pantalla completa
+
+  window.addEventListener("keydown", handleEscapeKey);
+
+  function handleEscapeKey(imageAction) {
+    if (imageAction.code === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", handleEscapeKey);
+    }
+  }
+}
+
+function action(imageAction) {
+  imageAction.preventDefault();
+}
+console.log(galleryItems);
